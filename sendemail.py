aaa64789dc40bfccd4e66649b07cce345b9cb77d
Anatolii <resident234@gmail.com>
2018-07-11 04:32:20 -0900

    Log in before trying to send anything
    Also, retrieve all the sensitive info from
    the environemt variables.

--- sendemail.py
+++ sendemail.py
@@ -6,9 +6,14 @@ from os import environ
 def send_message(body):
     message = MIMEText(body)
 
-    message['Subject'] += datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    message['Subject'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     message['To'] = environ['TO_EMAIL']
+    message['From'] = environ['FROM_EMAIL']
 
-    smtp_server = smtplib.SMTP('localhost')
+    smtp_server = smtplib.SMTP(environ['MAIL_HUB'])
+    smtp_server.starttls()
+    smtp_server.login(message['From'], environ['FROM_PASSWORD'])
     smtp_server.send_message(message)
     smtp_server.quit()
+
+send_message("This is a test")
